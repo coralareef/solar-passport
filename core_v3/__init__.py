2aa8c629c09b46b3fa2e52660cc643b5f5d1cb85
coralareef <133449047+coralareef@users.noreply.github.com>
2026-08-20 22:37:15 +0800

    Core v3: expose bankability assessment

--- core_v3/__init__.py
+++ core_v3/__init__.py
@@ -4,6 +4,7 @@ from .tariffs import BillResult, TariffEngine
 from .net_metering import NetMeteringLedger, SettlementMonth, settle_series
 from .intervals import IntervalPoint, IntervalParseReport, EnergyMatch, parse_load_csv, resample_hourly, match_load_and_pv, aggregate_by_month
 from .finance import ProjectFinanceInputs, ProjectFinanceResult, model_project_finance, solve_tariff, debt_capacity_for_dscr
+from .bankability import BankabilityAssessment, assess_bankability
 from .solar import PVWattsClient, PVWattsProfile, PVWattsError
 from .building import BuildingMonth, BuildingEnergyResult, analyze_hourly_building, pv_profile_to_typical_year_points
 
@@ -12,5 +13,6 @@ __all__ = [
     "BillResult", "TariffEngine", "NetMeteringLedger", "SettlementMonth", "settle_series",
     "IntervalPoint", "IntervalParseReport", "EnergyMatch", "parse_load_csv", "resample_hourly", "match_load_and_pv", "aggregate_by_month",
     "ProjectFinanceInputs", "ProjectFinanceResult", "model_project_finance", "solve_tariff", "debt_capacity_for_dscr",
+    "BankabilityAssessment", "assess_bankability",
     "PVWattsClient", "PVWattsProfile", "PVWattsError", "BuildingMonth", "BuildingEnergyResult", "analyze_hourly_building", "pv_profile_to_typical_year_points",
 ]
